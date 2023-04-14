00d1a1cb2309fe36ec3a32ece09f5e9ad8006a54
Jimmy-Du <jdu-dev@outlook.com>
2023-04-14 13:46:09 -0400

    Added recently released and coming soon sections

--- app/page.tsx
+++ app/page.tsx
@@ -18,21 +18,54 @@ async function getGames() {
 
 export default async function Home() {
   let games: Game[] = await getGames()
+  let today: Date = new Date()
   
   return (
     <div className='container'>
-      {
-        games.map((game) => {
-          return <UpcomingContent 
-                    key={game._id.toString()}
-                    _id={game._id}
-                    title={game.title} 
-                    releaseDate={game.releaseDate}
-                    image={game.image} 
-                    website={game.website}
-                    platforms={game.platforms} />
-        })
-      }
+      <h2>Recently Released</h2>
+      <div className='content-container'>
+        {
+          games.map((game) => {
+            // if the date of the game's release is less to today's date, display the
+            // game in the recently released section
+            if (new Date(game.releaseDate) < today) {
+              return <UpcomingContent 
+                        key={game._id.toString()}
+                        _id={game._id}
+                        title={game.title} 
+                        releaseDate={game.releaseDate}
+                        image={game.image} 
+                        website={game.website}
+                        platforms={game.platforms} />
+            }
+            else {
+              return null
+            }
+          })
+        }
+      </div>
+      <h2>Coming Soon</h2>
+      <div className='content-container'>
+        {
+          games.map((game) => {
+            // if the date of the game's release is greater than today's date, display the
+            // game in the coming soon section
+            if (new Date(game.releaseDate) > today) {
+              return <UpcomingContent 
+                        key={game._id.toString()}
+                        _id={game._id}
+                        title={game.title} 
+                        releaseDate={game.releaseDate}
+                        image={game.image} 
+                        website={game.website}
+                        platforms={game.platforms} />
+            }
+            else {
+              return null
+            }
+          })
+        }
+      </div>
     </div>
   )
 }
